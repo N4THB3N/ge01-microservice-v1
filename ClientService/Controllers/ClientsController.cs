a2f50cc6ba73c9a03dde7f46ae4005e4204ca476
Natan Castaneda <ncastanedahidalgo@gmail.com>
2026-06-08 15:52:58 -0600

    feat(exe): CRUD operations finished

--- ClientService/Controllers/ClientsController.cs
+++ ClientService/Controllers/ClientsController.cs
@@ -35,5 +35,50 @@ namespace ClientService.Controllers
 
             return CreatedAtAction(nameof(GetClients), new { id = client.ID }, client);
         }
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult<Client>> UpdateClient(int id, Client client)
+        {
+            if (id != client.ID)
+            {
+                return BadRequest("Client ID mismatch");
+            }
+
+            var existingClient = await _context.Clients.FindAsync(id);
+            if (existingClient == null)
+            {
+                return NotFound();
+            }
+
+            existingClient.First_Name = client.First_Name;
+            existingClient.Last_Name = client.Last_Name;
+            existingClient.Email = client.Email;
+            existingClient.Phone_Number = client.Phone_Number;
+            existingClient.Addr1 = client.Addr1;
+            existingClient.Municipality = client.Municipality;
+            existingClient.Department = client.Department;
+            existingClient.Occupation = client.Occupation;
+            existingClient.DOB = client.DOB;
+            existingClient.Updated_At = DateTime.UtcNow;
+
+            await _context.SaveChangesAsync();
+
+            return Ok(existingClient);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Client>> DeleteClient(int id)
+        {
+            var client = await _context.Clients.FindAsync(id);
+            if (client == null)
+            {
+                return NotFound();
+            }
+
+            _context.Clients.Remove(client);
+            await _context.SaveChangesAsync();
+
+            return Ok(client);
+        }
     }
 }
